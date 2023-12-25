ff2013929f820061c4ba850ec6a48d51f96a79f5
Theabhikaushik <128639550+Theabhikaushik@users.noreply.github.com>
2023-12-25 14:19:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var typed = new Typed('#element', {
+    strings: ['Web developer ', 'Video Editor', ' and a graphics designer.'],
+    typeSpeed: 50,
+});
+
+// <!-- // Light-darkmode script start here -->
+
+document.getElementById('toggleLabel').addEventListener('click', toggleDarkMode);
+
+        function toggleDarkMode() {
+            const body = document.body;
+            const icon = document.getElementById('icon');
+            const toggleCheckbox = document.getElementById('LightModeToggle');
+
+            body.classList.toggle('dark-mode');
+            const isDarkMode = body.classList.contains('dark-mode');
+
+            icon.textContent = isDarkMode ? '🌚' : '🌞 ';
+            toggleCheckbox.checked = isDarkMode;
+        }
+        // <!-- Scroll animation script  -->
+        AOS.init({
+            offset: 300,
+            duration: 700
+        });
